30a136efd569377ed6b2d2873ee9720d00d59057
Miroslav Stastny <mistastn@microsoft.com>
2020-08-26 11:58:16 +0200

    macOS dock size

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,3 +1,9 @@
 # Fast key repeat
 defaults write NSGlobalDomain InitialKeyRepeat -int 16
 defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Dock
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock show-recents -bool FALSE
+
+killall Dock
